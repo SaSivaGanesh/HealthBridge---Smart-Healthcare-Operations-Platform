fb3bb59c9896825982de1672ba5c41f2b62f6ae8
SaSivaGanesh <dronadulasaisivaganesh@gmail.com>
2026-04-26 16:43:30 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Vite + React</h1>
       <p>welcome to your Vite + React project! </p>
       <p> Edit <code>src/App.jsx</code> and save to test HMR updates.</p>
+      <p>kedari super queen</p>
     </>
   )
 }
