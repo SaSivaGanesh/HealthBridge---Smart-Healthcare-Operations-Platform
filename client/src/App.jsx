93166dc12980b0a4a2c3613bfb1c0ddfe6cf8304
SaSivaGanesh <dronadulasaisivaganesh@gmail.com>
2026-04-26 17:00:54 +0530

    added login page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <p> Edit <code>src/App.jsx</code> and save to test HMR updates.</p>
       <p>kedari super queen</p>
       <p>Super Women</p>
+      <p>Strong Women</p>
     </>
   )
 }
